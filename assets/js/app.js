f5e89510efd4eb0bdb63e84a52b9d3fd2230ceb3
Amaresh Parida <amresh.parida10000@gmail.com>
2023-01-20 05:49:50 +0530

    Katrina V3.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,17 @@ const btn = document.querySelector(".talk");
 const content = document.querySelector(".content");
 var voices = speechSynthesis.getVoices();
 function speak(sentence) {
-  document.getElementById("interface").src = "assets/img/katrina.gif";
+  document.getElementById("interface").src = "assets/img/katrina-2.gif";
 
   const text_speak = new SpeechSynthesisUtterance(sentence);
 
   text_speak.rate = 1;
   text_speak.pitch = 1;
   var voices = speechSynthesis.getVoices();
-  text_speak.voice = voices[3];
+  text_speak.voice = voices[55];
   window.speechSynthesis.speak(text_speak);
   text_speak.addEventListener("end", function () {
-    document.getElementById("interface").src = "assets/img/katrina.png";
+    document.getElementById("interface").src = "assets/img/katrina-2.png";
   });
 }
 
@@ -35,7 +35,7 @@ window.addEventListener("load", () => {
   speak("Activating Katrina");
   speak("Going online");
   wishMe();
-  document.getElementById("interface").src = "assets/img/katrina.png";
+  document.getElementById("interface").src = "assets/img/katrina-2.png";
 });
 
 const magic_word = "katrina";
@@ -50,34 +50,39 @@ recognition2.maxAlternatives = 1;
 recognition2.continuous = true;
 
 recognition2.onresult = (event) => {
+  document.getElementById("speak-mic").src = "assets/img/pause-mic.gif";
   var current = event.resultIndex;
   var transcript = event.results[current][0].transcript;
-  content.textContent = transcript.replace("Katrina", "");
+  content.innerHTML += contentStyle("You",transcript);
+  scrollSmoothToBottom('content-container');
   if (transcript.toLowerCase().includes(magic_word)) {
-    
+    stopSpeech();
     speakThis(transcript.toLowerCase().replace(magic_word, ""));
 
   }
 };
 
-// called when we detect silence
 function stopSpeech() {
+  document.getElementById("speak-mic").src = "assets/img/pause-mic.gif";
   recognition2.stop();
 }
 // called when we detect sound
 function startSpeech() {
   try {
     // calling it twice will throw...
+    document.getElementById("speak-mic").src = "assets/img/play-mic.gif";
     recognition2.start();
   } catch (e) {}
 }
 
 // request a LocalMediaStream
+/*
 navigator.mediaDevices
   .getUserMedia({ audio: true })
   // add our listeners
   .then((stream) => detectSilence(stream, stopSpeech, startSpeech))
   .catch((e) => log(e.message));
+
 
 function detectSilence(
   stream,
@@ -116,21 +121,23 @@ function detectSilence(
   loop();
 }
 
-
+*/
 
 function speakThis(message) {
-  document.getElementById("interface").src = "assets/img/katrina.gif";
   const speech = new SpeechSynthesisUtterance();
   var voices = speechSynthesis.getVoices();
-  speech.voice = voices[3];
+  speech.voice = voices[55];
+  speech.volume = 1;
+  speech.pitch = 1;
+  speech.rate = 1;
   speech.text = "I did not understand what you said please try again";
 
   if (message.includes("how are you")) {
     const finalText = "I am fine boss tell me how can i help you";
     speech.text = finalText;
-  } else if (message.includes("name") || message.includes("whats your name")) {
+  } else if (message.includes("whats your name")) {
     const finalText =
-      "My name is Katrina. I'm a intelligent virtual assitant 1.0";
+      "My name is Katrina. I'm a intelligent virtual assitant 3.0";
     speech.text = finalText;
   } else if (message.includes("open google")) {
     window.open("https://google.com", "_blank");
@@ -141,9 +148,7 @@ function speakThis(message) {
     const finalText = "Opening instagram";
     speech.text = finalText;
   } else if (
-    message.includes("what is") ||
-    message.includes("who is") ||
-    message.includes("what are")
+    message.includes("search google") 
   ) {
     window.open(
       `https://www.google.com/search?q=${message.replace(" ", "+")}`,
@@ -158,25 +163,11 @@ function speakThis(message) {
     );
     const finalText = "This is what i found on wikipedia regarding " + message;
     speech.text = finalText;
-  } else if (message.includes("time")) {
-    const time = new Date().toLocaleString(undefined, {
-      hour: "numeric",
-      minute: "numeric",
-    });
-    const finalText = time;
-    speech.text = finalText;
-  } else if (message.includes("date")) {
-    const date = new Date().toLocaleString(undefined, {
-      month: "short",
-      day: "numeric",
-    });
-    const finalText = date;
-    speech.text = finalText;
-  } else if (message.includes("calculator")) {
+  }   else if (message.includes("open calculator")) {
     window.open("Calculator:///");
     const finalText = "Opening Calculator";
     speech.text = finalText;
-  } else if (message.includes("youtube")) {
+  } else if (message.includes("search youtube")) {
     window.open(
       `https://www.youtube.com/results?search_query=${message.replace(
         "youtube",
@@ -207,13 +198,6 @@ function speakThis(message) {
     window.open("tel:+916294915441", "_blank");
     const finalText = "Calling " + message.split(" ")[1];
     speech.text = finalText;
-  } else if (
-    message.includes("call jayesh sir") ||
-    message.includes("call jayesh")
-  ) {
-    window.open("tel:+917735746580", "_blank");
-    const finalText = "Calling " + message.split(" ")[1];
-    speech.text = finalText;
   } else {
       if(message == "")
       {
@@ -222,22 +206,96 @@ function speakThis(message) {
       }
       else
       {
-        window.open(
-            `https://www.google.com/search?q=${message.replace(" ", "+")}`,
-            "_blank"
-          );
-          const finalText = "I found some information for " + message + " on google";
-          speech.text = finalText;
+        
+         
+
+          getChatGPTData(message).then(res => {
+            let finalText = res;
+            if(finalText == "Error:408" || finalText == "404"){
+              window.open(
+                `https://www.google.com/search?q=${message.replace(" ", "+")}`,
+                "_blank"
+              );
+              finalText = "I found some information for " + message + " on google";
+            }
+            speech.text = finalText;
+            document.getElementById("interface").src = "assets/img/katrina-2.gif";
+            window.speechSynthesis.speak(speech);
+            content.innerHTML += contentStyle("Katrina", speech.text);
+            scrollSmoothToBottom('content-container');
+            speech.addEventListener("end", function () {
+              document.getElementById("interface").src = "assets/img/katrina-2.png";
+            });
+          });
+
+          return;
       }
     
   }
 
-  speech.volume = 1;
-  speech.pitch = 1;
-  speech.rate = 1;
 
+  document.getElementById("interface").src = "assets/img/katrina-2.gif";
   window.speechSynthesis.speak(speech);
   speech.addEventListener("end", function () {
-    document.getElementById("interface").src = "assets/img/katrina.png";
+    document.getElementById("interface").src = "assets/img/katrina-2.png";
   });
 }
+
+function contentStyle(x, str){
+  return '<br/><b style="color: #fff;">'+x+':</b> <i>'+str+'</i>';
+}
+
+function scrollSmoothToBottom(id){
+  const element = $(`#${id}`);
+  element.animate({
+     scrollTop: element.prop("scrollHeight")
+  }, 500);
+}
+
+function askViaText(){
+  let message = document.getElementById('askterm').value;
+  document.getElementById('askterm').value = "";
+  if(message != "" && message.length >0){
+    speakThis(message.toLowerCase().trim());
+  }
+}
+
+
+document.getElementById('askterm').onkeydown = function(e){
+  if(e.keyCode == 13){
+    askViaText();
+  }
+};
+
+//Open.AI ChatGPT Integration 
+
+const getChatGPTData = async (message) => {
+  var data = {
+    model: "text-davinci-003",
+    prompt: message,
+    max_tokens: 2048,
+    user: "1",
+    temperature:  0.5,
+    frequency_penalty: 0.0, 
+    presence_penalty: 0.0,  
+    stop: ["#", ";"] 
+  }
+  const request = await fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer 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",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)  
+  });
+  const res = await request.json();
+  var returnValue = "";
+  if (res.error && res.error.message) {
+    returnValue = "Error:408";
+  } else if (res.choices && res.choices[0].text) {
+     returnValue = res.choices[0].text;
+     if (returnValue == "") returnValue = "404";
+     return returnValue;
+  }       
+};
